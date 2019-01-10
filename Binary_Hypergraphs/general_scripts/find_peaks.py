d9c54999e74a8eb77f5d0f4a8dc58c10ccb412b3
ThomasGreen <thomasgreen@work-machine.boisestate.edu>
2019-01-10 13:34:13 -0700

    Added data files for nk problem - 13, 14, and 15 bit spaces all with kval 3. Added usage line to find_peaks script.

--- Binary_Hypergraphs/general_scripts/find_peaks.py
+++ Binary_Hypergraphs/general_scripts/find_peaks.py
@@ -1,10 +1,14 @@
 #Given a SAN csv file, find any node that is connected to itself. These will be the peaks of the lanscape
 import sys
 
-filename = sys.argv[1]
-f = open(filename, "r")
+if len(sys.argv) < 2:
+  print("Usage: python find_paths.py <network_filename>")
+  sys.exit(0)
 
-for line in f:
+network_filename = sys.argv[1]
+nf = open(network_filename, "r")
+
+for line in nf:
   nodes = line.split(";")
   if int(nodes[0]) == int(nodes[1]):
     print(str(nodes[0]))
